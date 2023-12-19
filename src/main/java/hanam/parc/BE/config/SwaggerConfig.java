21a176a67c71d706cf6c88d65af4c12d021ecede
LeeHanEum <LeeHanEum160924@outlook.kr>
2023-12-19 17:16:14 +0900

    feat : swagger 세팅

---
+++ src/main/java/hanam/parc/BE/config/SwaggerConfig.java
@@ -0,0 +1,21 @@
+package hanam.parc.BE.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI openAPI() {
+        Info info  = new Info()
+                .title("하남시 장애인 체육회 api")
+                .description("하남시 장애인 체육회 api 문서입니다.")
+                .version("v1.0.0");
+        return new OpenAPI()
+                .components(new Components())
+                .info(info);
+    }
+}
